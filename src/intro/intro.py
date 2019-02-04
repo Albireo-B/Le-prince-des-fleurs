c9adb22cc96262e209297d66139ef344e1f7c1c2
Alexis Gros <alexis.gros99@gmail.com>
2019-02-04 15:24:32 +0100

    animation

--- src/intro/intro.py
+++ src/intro/intro.py
@@ -1,4 +1,5 @@
 import pygame
+import time
 
 pygame.init()
 display_width=800
@@ -12,32 +13,23 @@ clock = pygame.time.Clock()
 
 crashed = False
 
-carImg = pygame.image.load('/users/info/etu-s3/deniaul/Morpion/Jeu_morpion/Morpion/src/morpion/images/jouer.png')
-
 animation = {1:1,2:4,3:2,4:1,3:3,4:1,3:1,4:2,1:8,5:1,6:2,7:1,8:1,9:1,10:2,11:1,12:1,13:1,14:2,15:1,16:1,17:1,18:1,19:2,20:1,21:2,19:2,22:1,21:11}
 
-def car(x,y):
-    gameDisplay.blit(carImg, (x,y))
+def readIntro(fenetre):
+    width, heigh = pygame.display.get_surface().get_size()
+    tabFrame = {1:1,2:4,3:2,4:1,3:3,4:1,3:1,4:2,1:8,5:1,6:2,7:1,8:1,9:1,10:2,11:1,12:1,13:1,14:2,15:1,16:1,17:1,18:1,19:2,20:1,21:2,19:2,22:1,21:11}
+    imgList = {}
+    for i in range(1,24):
+        img = pygame.image.load("img/"+str(i)+".png").convert()
+        formattedImg = pygame.transform.scale(img.subsurface((150, 30, 900, 650)), (100, 100))
+        imgList[i] = formattedImg
 
-x =  (display_width / 3)
-y = (display_height / 2)
+    for key in tabFrame:
+        fenetre.blit(imgList[key], (width/2,heigh/2))
+        pygame.display.flip()
+        time.sleep(tabFrame[key]/24)
 
-y_change = 0
 pygame.mixer.Sound.play(logo_sound)
-while not crashed:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            crashed = True
-            ############################
-    if pygame.time.get_ticks()>2500:
-        y_change = -5
-        print('bob')
-        ########################
-    y += y_change
-    ##
-    gameDisplay.fill(white)
-    car(x,y)
-    pygame.display.update()
-    clock.tick(60)
+readIntro(gameDisplay)
 pygame.quit()
 quit()
