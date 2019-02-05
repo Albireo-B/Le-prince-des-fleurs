a82dc7d3db019cf57cc142d3ae28a99fdc2f76b6
VINET Guillaume <vinetg@hostname.iut2.upmf-grenoble.fr>
2019-02-05 11:05:08 +0100

    Fin du codage des méthodes de display et de rotation des volcans et planetes

--- src/Controllers/GameController.py
+++ src/Controllers/GameController.py
@@ -1,22 +1,52 @@
 import pygame
 
+pygame.init()
+
 class GameController:
 
 
     def __init__(self,vueScreen):
         self.vueScreen=vueScreen()
-
+        self.planetes={}
+        self.prince=Prince()
 
     def createAndRotatePlanet(imgPath,size,centerPositionx,centerPositiony,rotationAngle):
-
         planet = Planet(imgPath,size,centerPosition,rotationAngle)
+        self.planetes[self.planetes.get_size()]=planet
+
+
+    def addPrinceOnPlanet(planet):
+        planet.addPrince(prince)
+
+    def removePrinceFromPlanet(planet):
+        planet.removePrince(prince)
+
+#createAndRotatePlanet("../../images/planèteTest.jpg",(200,200),375,375,2)
+
+
+
+    def init_display():
+        #couleur blanche a virer
         vueScreen.fill((255,255,255))
-        vueScreen.blit(planet.imgPlanet,(planet.planetCenter))
-        vueScreen.blit(planet.imgVolcan,(planet.volcanoCenter))
-        pygame.display.update()
-        clock.tick(30)
+        for key in planetes:
+            vueScreen.blit(planetes[key].imgPlanet,(planetes[key].planetCenter))
+            vueScreen.blit(planetes[key].imgVolcan,(planetes[key].volcanos.volcanoCenter))
+        init_rotate()
 
-    def addVolcanoOnPlanet(imgPath,planet):
-        planet.addVolcano(imgPath,planet.imgPlanet)
 
-    
+    def init_rotate():
+        done=False
+        while not done:
+            for event in pygame.event.get():
+                if event.type == pygame.quit:
+                    done=True
+            for key in planetes:
+                imgPlaneteCopie=planetes[key].imgPlanet.copy()
+                imgVolcanCopie=planetes[key].volcano.imgVolcano.copy()
+                rectplanet = planetes[key].imgPlanet.get_rect(center=(centerPositionx,centerPositiony))
+                angle += planetes[key].rotationAngle
+                planetes[key].imgPlanet=pygame.transform.rotozoom(imgPlaneteCopie,angle,1)
+                planetCenter = planetes[key].imgPlanet.get_rect(center=rectplanet.center)
+                planetes[key].volcano.imgVolcano=pygame.transform.rotozoom(imgVolcanCopie,angle,1)
+            pygame.display.update()
+            clock.tick(30)
--- src/Objects/Planet.py
+++ src/Objects/Planet.py
@@ -8,42 +8,26 @@ class Planet:
         self.size=size
         self.imgPlanet=pygame.image.load(self.imgPath)
         self.rotationAngle=rotationAngle
-        self.imgVolcan=None
-        self.volcanoCenter=None
+        self.withPrince=False
+        self.volcano=Volcano("../../../images/volcan.png",centerPositionx,centerPositiony)
+
         #change la taille de l'image
         self.imgPlanet=pygame.transform.scale(self.imgPlanet,self.size)
+        self.volcano.imgVolcano=pygame.transform.scale(self.volcano.imgVolcano,(250,250))
 
-        imgPlaneteCopie=self.imgPlanet.copy()
+        self.volcano.imgVolcano=pygame.transform.rotate(self.volcano.imgVolcano,-85)
 
-        rectplanet = self.imgPlanet.get_rect(center=(centerPositionx,centerPositiony))
 
-        angle = 0
 
-        done=False
-        while not done:
-            for event in pygame.event.get():
-                if event.type == pygame.quit:
-                    done=True
-            angle += self.rotationAngle
-            self.imgPlanet=pygame.transform.rotozoom(imgPlaneteCopie,angle,1)
-            planetCenter = self.imgPlanet.get_rect(center=rectplanet.center)
 
-    def addVolcanoOnPlanet(imgPath,imgPlanet):
-        self.imgVolcan=pygame.image.load(imgPath)
-        self.imgVolcan=pygame.transform.rotate(self.imgVolcan,-85)
 
-        #change la taille de l'image
-        self.imgVolcan=pygame.transform.scale(self.imgVolcan,(250,250))
+    def addPrince():
 
-        imgVolcanCopie=self.imgVolcan.copy()
 
-        angle = 0
-        done=False
-        while not done:
-            for event in pygame.event.get():
-                if event.type == pygame.quit:
-                    done=True
-            angle += self.rotationAngle
-            self.imgVolcan=pygame.transform.rotozoom(imgVolcanCopie,angle,1)
-            rectVolcano = self.imgVolcan.get_rect(center=(self.centerPositionx+math.cos(math.radians(-angle))*200,self.centerPositiony+math.sin(math.radians(-angle))*200))
-            self.volcanoCenter = self.imgVolcan.get_rect(center=rectVolcano.center)
+        self.withPrince=True
+
+
+    def removePrince():
+
+
+        self.withPrince=False
