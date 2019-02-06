6b2c554b2b2555507356d68b79a9a556bf0ada2c
GENEVOIS Clement <genevoic@hostname.iut2.upmf-grenoble.fr>
2019-02-06 17:54:13 +0100

    ajout du menu flottant

--- src/Controllers/MenuController.py
+++ src/Controllers/MenuController.py
@@ -10,42 +10,45 @@ skyblue = (135,206,235)
 class MenuController:
 
     def __init__(self, screen):
+        i=0
+        j=0
         white= (255,255,255)
-        screen.fill(white)
+
         fortgroud = pygame.image.load('../images/menu.png').convert()
         background = pygame.image.load('../images/menu.png').convert_alpha()
         myFont = pygame.font.SysFont('arial',38)
-        start  = myFont.render("Start",True,[135,206,235])
+        start  = myFont.render("start",True,[135,206,235])
         button_rect_start=start.get_rect(topleft=(50,200))
-        scores = myFont.render("Scores",True,[135,206,235])
+        #button_rect_start=start.fill(black)
+        scores = myFont.render("scores",True,[135,206,235])
         button_rect_scores=scores.get_rect(topleft=(50,300))
-        credits= myFont.render("Credits",True,[135,206,235])
+        credits= myFont.render("credits",True,[135,206,235])
         button_rect_credits=credits.get_rect(topleft=(50,400))
-        quit   = myFont.render("Quit",True,[135,206,235])
+        quit   = myFont.render("quit",True,[135,206,235])
         button_rect_quit=quit.get_rect(topleft=(50,500))
         pygame.mixer.music.load ('../Sounds/menu.wav')
         pygame.mixer.music.play(-1)
         while True:
-            screen.fill((255,255,255))
+            screen.fill(white)
             for event in pygame.event.get():
                 if event.type==QUIT:
                     pygame.quit()
-                if event.type==pygame.MOUSEBUTTONDOWN:
+                if event.type== pygame.MOUSEBUTTONDOWN:
                     pygame.mouse.get_pos()
                     if button_rect_start.collidepoint(event.pos):#event to be changed
                         pygame.mixer.music.stop()
-                        self.runGame()
+                        self.run()
                     if button_rect_scores.collidepoint(event.pos):#event to be changed
-                        pygame.mixer.music.stop()
-                        self.runScores()
+                        print('Button pressed.')
+                        sys.exit()
                     if button_rect_credits.collidepoint(event.pos):#event to be changed
-                        pygame.mixer.music.stop()
+                        print('Button pressed.')
                         sys.exit()
                     if button_rect_quit.collidepoint(event.pos):#event to be changed
-                        pygame.mixer.music.stop()
+                        print('Button pressed.')
                         sys.exit()
 
-
+            print(i)
             # position of buttons can be changed
             screen.blit(start,(50,200))
 
@@ -55,7 +58,17 @@ class MenuController:
 
             screen.blit(quit,(50,500))
 
-            screen.blit(background,(400,0))
+            if i<=30:
+                screen.blit(background, (400,30-i))
+            else:
+                screen.blit(background, (400,0+j))
+                j+=0.1
+            if j>30:
+                screen.blit(background, (400,0+j))
+                i=0
+                j=0
+    #screen.blit(background,(400,0))
+            i+=0.1
 
             pygame.display.update()
 
@@ -63,5 +76,5 @@ class MenuController:
         textSurface = font.render(text, True, black)
         return textSurface, textSurface.get_rect()
 
-    def runGame(self):
+    def run(self):
         controleur = GameController()
