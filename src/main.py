390f920fd4bf678fa0024f4c8b24a1a34c33dc29
leptiphoenix <35071358+leptiphoenix@users.noreply.github.com>
2019-02-08 02:30:48 +0100

    Les scores sont là bordel !

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from Controllers.MenuController import MenuController
 
 def main():
     pygame.init()
+
     gameDisplay = pygame.display.set_mode((1024,768))
     pygame.display.set_caption('Le Prince Des Fleurs')
     #launchIntro(gameDisplay)
