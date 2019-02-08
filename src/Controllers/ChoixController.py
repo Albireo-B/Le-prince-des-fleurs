212c14715d7e077824e7140e1cfca8016e7a19ff
sd621209 <40350821+sd621209@users.noreply.github.com>
2019-02-08 02:48:58 +0100

    Ajout de textes explicatifs pour les niveaux et correction du bug des fleurs

--- src/Controllers/ChoixController.py
+++ src/Controllers/ChoixController.py
@@ -1,53 +1,158 @@
-import pygame
+import pygame,sys
 from pygame.locals import *
 from Controllers.GameController import GameController
 from Controllers.ScoreController import ScoreController
-
 white = (255,255,255)
 black = (0,0,0)
 skyblue = (135,206,235)
 
+
+
 class ChoixController:
 
     def __init__(self, screen):
+
         i=0
         j=0
         white= (255,255,255)
         self.screen = screen
+        self.myFont = pygame.font.SysFont('arial',38)
+        self.message=self.myFont.render("",True,white)
         self.score=0
         self.fortgroud = pygame.image.load('../images/menu.png').convert_alpha()
-        myFont = pygame.font.SysFont('arial',38)
-        choix  = myFont.render("Choisissez votre niveau : ",True,[135,206,235])
-        niv1 = myFont.render("Niveau 1",True,[135,206,235])
+        choix  = self.myFont.render("Choisissez votre niveau : ",True,[135,206,235])
+        niv1 = self.myFont.render("Niveau 1",True,[135,206,235])
         button_rect_niv1=niv1.get_rect(topleft=(50,300))
-        niv2= myFont.render("Niveau 2",True,[135,206,235])
+        niv2= self.myFont.render("Niveau 2",True,[135,206,235])
         button_rect_niv2=niv2.get_rect(topleft=(50,400))
-        niv3   = myFont.render("Niveau 3",True,[135,206,235])
+        niv3   = self.myFont.render("Niveau 3",True,[135,206,235])
         button_rect_niv3=niv3.get_rect(topleft=(50,500))
-        retour   = myFont.render("Retour",True,[135,206,235])
+        retour   = self.myFont.render("Retour",True,[135,206,235])
         button_rect_retour=retour.get_rect(topleft=(50,600))
         self.background=pygame.image.load("../images/background.jpg").convert()
         self.background=pygame.transform.scale(self.background,(1024,768))
+        self.message1=""
+        self.message1=self.myFont.render(self.message1,True,white)
+        self.message2=""
+        self.message2=self.myFont.render(self.message2,True,white)
+        self.message3=""
+        self.message3=self.myFont.render(self.message3,True,white)
+        self.message4=""
+        self.message4=self.myFont.render(self.message4,True,white)
+        self.message5=""
+        self.message5=self.myFont.render(self.message5,True,white)
+        self.message6=""
+        self.message6=self.myFont.render(self.message6,True,white)
+        self.message7=""
+        self.message7=self.myFont.render(self.message7,True,white)
+        self.message8=""
+        self.message8=self.myFont.render(self.message8,True,white)
         while True:
             self.screen.blit(self.background,(0,0))
+            if i<=30:
+                screen.blit(self.fortgroud, (400,130-i))
+            else:
+                self.screen.blit(self.fortgroud, (400,100+j))
+                j+=0.05
+            if j>30:
+                self.screen.blit(self.fortgroud, (400,100+j))
+                i=0
+                j=0
+            i+=0.05
+            self.screen.blit(self.message1,(480,330))
+            self.screen.blit(self.message2,(465,370))
+            self.screen.blit(self.message3,(450,410))
+            self.screen.blit(self.message4,(435,450))
+            self.screen.blit(self.message5,(450,490))
+            self.screen.blit(self.message6,(465,530))
+            self.screen.blit(self.message7,(480,570))
+            self.screen.blit(self.message8,(495,610))
             for event in pygame.event.get():
-                if event.type==QUIT:
-                    pygame.quit()
-                if event.type== pygame.MOUSEBUTTONDOWN:
+                if event.type==pygame.MOUSEMOTION:
+                    pygame.mouse.get_pos()
+                    if button_rect_niv1.collidepoint(event.pos):#event to be changed
+                        self.message1=("Ce premier niveau n'a")
+                        self.message1=self.myFont.render(self.message1,True,white)
+                        self.message2=("pas de volcans, un nombre")
+                        self.message2=self.myFont.render(self.message2,True,white)
+                        self.message3=("faible de planetes et pas")
+                        self.message3=self.myFont.render(self.message3,True,white)
+                        self.message4=("de planetes a antigravite.")
+                        self.message4=self.myFont.render(self.message4,True,white)
+                        self.message5=("Il permet de s'entrainer ")
+                        self.message5=self.myFont.render(self.message5,True,white)
+                        self.message6=("aux mecaniques de saut,")
+                        self.message6=self.myFont.render(self.message6,True,white)
+                        self.message7=(" de gravite, des fleurs")
+                        self.message7=self.myFont.render(self.message7,True,white)
+                        self.message8=("et des etoiles.")
+                        self.message8=self.myFont.render(self.message8,True,white)
+                    elif button_rect_niv2.collidepoint(event.pos):#event to be changed
+                        self.message1=("Ce deuxieme niveau n'a")
+                        self.message1=self.myFont.render(self.message1,True,white)
+                        self.message2=("pas de volcans, un nombre")
+                        self.message2=self.myFont.render(self.message2,True,white)
+                        self.message3=("normal de planetes et ")
+                        self.message3=self.myFont.render(self.message3,True,white)
+                        self.message4=("une planete a antigravite.")
+                        self.message4=self.myFont.render(self.message4,True,white)
+                        self.message5=("Il permet de s'entrainer ")
+                        self.message5=self.myFont.render(self.message5,True,white)
+                        self.message6=("aux mecaniques de saut,")
+                        self.message6=self.myFont.render(self.message6,True,white)
+                        self.message7=(" de gravite et d'antigravite, ")
+                        self.message7=self.myFont.render(self.message7,True,white)
+                        self.message8=(" des fleurs et des etoiles.")
+                        self.message8=self.myFont.render(self.message8,True,white)
+                    elif button_rect_niv3.collidepoint(event.pos):#event to be changed
+                        self.message1=("Ce troisieme niveau a")
+                        self.message1=self.myFont.render(self.message1,True,white)
+                        self.message2=("tous les volcans, un nombre")
+                        self.message2=self.myFont.render(self.message2,True,white)
+                        self.message3=("normal de planetes et ")
+                        self.message3=self.myFont.render(self.message3,True,white)
+                        self.message4=("une planete a antigravite.")
+                        self.message4=self.myFont.render(self.message4,True,white)
+                        self.message5=("Il permet de profiter ")
+                        self.message5=self.myFont.render(self.message5,True,white)
+                        self.message6=("de toutes les mecaniques ")
+                        self.message6=self.myFont.render(self.message6,True,white)
+                        self.message7=("differentes du jeu et du")
+                        self.message7=self.myFont.render(self.message7,True,white)
+                        self.message8=("systeme de competitition.")
+                        self.message8=self.myFont.render(self.message8,True,white)
+                    else:
+                        self.message1=("")
+                        self.message1=self.myFont.render(self.message1,True,white)
+                        self.message2=("")
+                        self.message2=self.myFont.render(self.message2,True,white)
+                        self.message3=("")
+                        self.message3=self.myFont.render(self.message3,True,white)
+                        self.message4=("")
+                        self.message4=self.myFont.render(self.message4,True,white)
+                        self.message5=("")
+                        self.message5=self.myFont.render(self.message5,True,white)
+                        self.message6=("")
+                        self.message6=self.myFont.render(self.message6,True,white)
+                        self.message7=("")
+                        self.message7=self.myFont.render(self.message7,True,white)
+                        self.message8=("")
+                        self.message8=self.myFont.render(self.message8,True,white)
+                elif event.type== pygame.MOUSEBUTTONDOWN:
                     pygame.mouse.get_pos()
                     if button_rect_niv1.collidepoint(event.pos):#event to be changed
                         pygame.mixer.music.stop()
                         self.run(1)
                         self.score= ScoreController(self.gameController.score)
-                    if button_rect_niv2.collidepoint(event.pos):#event to be changed
+                    elif button_rect_niv2.collidepoint(event.pos):#event to be changed
                         pygame.mixer.music.stop()
                         self.run(2)
                         self.score= ScoreController(self.gameController.score)
-                    if button_rect_niv3.collidepoint(event.pos):#event to be changed
+                    elif button_rect_niv3.collidepoint(event.pos):#event to be changed
                         pygame.mixer.music.stop()
                         self.run(3)
                         self.score= ScoreController(self.gameController.score)
-                    if button_rect_retour.collidepoint(event.pos):#event to be changed
+                    elif button_rect_retour.collidepoint(event.pos):#event to be changed
                         return
 
             # position of buttons can be changed
@@ -56,19 +161,14 @@ class ChoixController:
             screen.blit(niv2,(50,400))
             screen.blit(niv3,(50,500))
             screen.blit(retour,(50,600))
-
-            if i<=30:
-                screen.blit(self.fortgroud, (400,130-i))
-            else:
-                screen.blit(self.fortgroud, (400,100+j))
-                j+=0.05
-            if j>30:
-                screen.blit(self.fortgroud, (400,100+j))
-                i=0
-                j=0
-            i+=0.05
-
             pygame.display.update()
+
+
+
+
+
+
+
 
     def run(self,niv):
         self.gameController = GameController(self.screen,True,niv)
--- src/Controllers/MenuController.py
+++ src/Controllers/MenuController.py
@@ -22,16 +22,16 @@ class MenuController:
         self.screen = screen
         self.fortgroud = pygame.image.load('../images/menu.png').convert_alpha()
         myFont = pygame.font.SysFont('arial',38)
-        start  = myFont.render("Start",True,[135,206,235])
+        start  = myFont.render("Jouer",True,[135,206,235])
         button_rect_start=start.get_rect(topleft=(50,200))
         #button_rect_start=start.fill(black)
         scores = myFont.render("Scores",True,[135,206,235])
         button_rect_scores=scores.get_rect(topleft=(50,300))
         credits= myFont.render("Credits",True,[135,206,235])
         button_rect_credits=credits.get_rect(topleft=(50,400))
-        quit   = myFont.render("Quit",True,[135,206,235])
+        quit   = myFont.render("Quitter",True,[135,206,235])
         button_rect_quit=quit.get_rect(topleft=(50,600))
-        rules   = myFont.render("Rules",True,[135,206,235])
+        rules   = myFont.render("Regles",True,[135,206,235])
         button_rect_rules=quit.get_rect(topleft=(50,500))
         pygame.mixer.music.load ('../Sounds/menu.wav')
         pygame.mixer.music.play(-1)
@@ -47,6 +47,7 @@ class MenuController:
                     if button_rect_start.collidepoint(event.pos):#event to be changed
                         pygame.mixer.music.stop()
                         choixController=ChoixController(screen)
+                        self.score = ScoreController(choixController.score)
                     if button_rect_scores.collidepoint(event.pos):#event to be changed
                         self.score = ScoreController(None)
                         self.scores(screen)
