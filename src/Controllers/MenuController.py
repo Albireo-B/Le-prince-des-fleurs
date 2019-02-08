f4ff757f77538ba49b1a8ae7982dc687b11dbfc3
leptiphoenix <35071358+leptiphoenix@users.noreply.github.com>
2019-02-08 05:12:03 +0100

    nouveau bouton

--- src/Controllers/MenuController.py
+++ src/Controllers/MenuController.py
@@ -22,15 +22,17 @@ class MenuController:
         self.fortgroud = pygame.image.load('../images/menu.png').convert_alpha()
         myFont = pygame.font.SysFont('arial',38)
         start  = myFont.render("Jouer",True,[135,206,235])
-        button_rect_start=start.get_rect(topleft=(50,200))
+        button_rect_start=start.get_rect(topleft=(50,175))
         scores = myFont.render("Scores",True,[135,206,235])
-        button_rect_scores=scores.get_rect(topleft=(50,300))
+        button_rect_scores=scores.get_rect(topleft=(50,275))
         credits= myFont.render("Credits",True,[135,206,235])
-        button_rect_credits=credits.get_rect(topleft=(50,400))
+        button_rect_credits=credits.get_rect(topleft=(50,375))
         quit   = myFont.render("Quitter",True,[135,206,235])
-        button_rect_quit=quit.get_rect(topleft=(50,600))
+        button_rect_quit=quit.get_rect(topleft=(50,675))
         rules   = myFont.render("Règles",True,[135,206,235])
-        button_rect_rules=quit.get_rect(topleft=(50,500))
+        button_rect_rules=quit.get_rect(topleft=(50,475))
+        lore   = myFont.render("histoire",True,[135,206,235])
+        button_rect_lore=quit.get_rect(topleft=(50,575))
         pygame.mixer.music.load ('../Sounds/menu.wav')
         pygame.mixer.music.play(-1)
         self.background=pygame.image.load("../images/background.jpg").convert()
@@ -53,16 +55,19 @@ class MenuController:
                     if button_rect_credits.collidepoint(event.pos):#event to be changed
                         self.run2()
                     if button_rect_rules.collidepoint(event.pos):#event to be changed
+                        print("oups")
+                    if button_rect_lore.collidepoint(event.pos):#event to be changed
                         self.rules(screen)
                     if button_rect_quit.collidepoint(event.pos):#event to be changed
                         sys.exit()
 
             # position of buttons can be changed
-            screen.blit(start,(50,200))
-            screen.blit(scores,(50,300))
-            screen.blit(credits,(50,400))
-            screen.blit(rules,(50,500))
-            screen.blit(quit,(50,600))
+            screen.blit(start,(50,175))
+            screen.blit(scores,(50,275))
+            screen.blit(credits,(50,375))
+            screen.blit(rules,(50,475))
+            screen.blit(lore,(50,575))
+            screen.blit(quit,(50,675))
 
             screen.blit(self.fortgroud, (400,100 + math.sin(i)*20))
             i+=.002
