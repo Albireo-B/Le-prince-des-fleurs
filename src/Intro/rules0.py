2042bbb348b7de9843f44183874dc68cc07bae40
leptiphoenix <35071358+leptiphoenix@users.noreply.github.com>
2019-02-08 05:13:08 +0100

    oups

---
+++ src/Intro/rules0.py
@@ -0,0 +1,43 @@
+import pygame
+import time
+from pygame.locals import *
+import math
+
+def launchRules(screen):
+    i=0
+    j=0
+    blue= (0,90,255)
+    fortgroud = pygame.image.load('../images/Planet1.png').convert()
+    background = pygame.image.load('../images/Planet1.png').convert_alpha()
+    background3=pygame.image.load("../images/background.jpg").convert()
+    background3=pygame.transform.scale(background3,(1024,768))
+    myFont = pygame.font.SysFont('arial',38)
+    mytext = pygame.font.SysFont('arial',28)
+
+
+    backtoMenu  = myFont.render("Retour",True,[135,206,235])
+    text = pygame.image.load("../images/text.png").convert_alpha()
+
+
+    #here we put all the rules of the game
+
+    button_rect_backtoMenu=backtoMenu.get_rect(topleft=(50,700))
+
+    while True:
+        screen.blit(background3,(0,0))
+        for event in pygame.event.get():
+            if event.type==QUIT:
+                pygame.quit()
+            if event.type== pygame.MOUSEBUTTONDOWN:
+                pygame.mouse.get_pos()
+                if button_rect_backtoMenu.collidepoint(event.pos):#event to be changed
+                    return
+
+        # position of buttons can be changed
+        i=420
+        background2 = pygame.transform.scale(background, (600+i,592+i))
+        screen.blit(text, (0,0))
+
+
+
+        pygame.display.update()
